4fa833be8263138d58ef581cfa62496883c80aa1
atligudmunds <a13@ru.is>
2014-05-05 16:05:22 +0000

    For testing, please ignore

---
+++ SubHub/Models/Atli_Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubHub.Models
+{
+    public class Atli_Test
+    {
+        public void PrentaTexta()
+        {
+            System.Console.WriteLine("Þetta er test frá honum Atla");
+        }
+    }
+}
