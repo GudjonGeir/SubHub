0d9d371eb7f1488f27dfd4c14a0562b9c7d12444
katrins11 <katrins11@ru.is>
2014-05-05 15:11:55 +0000

    added few classes

---
+++ SubHub/Models/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubHub.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string DateSubmitted { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ SubHub/Models/RequestRating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubHub.Models
+{
+    public class RequestRating
+    {
+        public int Id { get; set; }
+        public List<int> UserId { get; set; }
+            public int count {get; set; }
+    }
+}
