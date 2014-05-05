09212d9fa262c661f12e58da6eecd464c215c2ef
GudjonGeir <gudjongeir@gmail.com>
2014-05-05 14:22:56 +0000

    Added Subtitle class

---
+++ SubHub/Models/Subtitle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubHub.Models
+{
+    public class Subtitle
+    {
+        public int Id { get; set; }
+        public string Language { get; set; }
+        public string Type { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
