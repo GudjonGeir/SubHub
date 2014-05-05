4d0ed82f71538b7ab5d23e253cb4d797f06997e5
GudjonGeir <gudjongeir@gmail.com>
2014-05-05 15:03:59 +0000

    Added classes Comment, SubtitleLine and SubtitleRating

--- SubHub/Models/Subtitle.cs
+++ SubHub/Models/Subtitle.cs
@@ -11,6 +11,12 @@ namespace SubHub.Models
         public string Language { get; set; }
         public string Type { get; set; }
         public string Genre { get; set; }
+        public string Name { get; set; }
+        public string ImdbUrl { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public DateTime DateAired { get; set; }
+        public string PosterUrl { get; set; }
+        public List<int> EditedBy { get; set; }
 
     }
 }
