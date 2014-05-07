d3ae064cb169514c5f787c37f3e77e02490313cc
steinaragust <steinar13@ru.is>
2014-05-07 09:38:53 +0000

    sdf

---
+++ SubHub/Repositories/RequestRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SubHub.Model;
+
+namespace SubHub.Repositories
+{
+    public class RequestRepository : ISubtitleRepository
+    {
+        //private static RequestRepository _instance;
+
+        //private static RequestRepository Instance
+        //{
+        //    get
+        //    {
+        //        if (_instance == null)
+        //            _instance = new RequestRepository();
+        //        return _instance;
+        //    }
+        //}
+        //private List<Request> m_requests = null;
+
+        //public IQueryable<Request> GetRequests()
+        //{
+        //    //var result = from c in m_users
+        //    //             orderby c.DateSubmitted ascending
+        //    //             select c;
+        //    return result;
+        //}
+
+        //public void AddRequest(Request c)
+        //{
+        //    int newId = 1;
+        //    if (m_requests.Count() > 0)
+        //    {
+        //        newId = m_requests.Max(x => x.ID) + 1;
+        //    }
+        //    c.Id = newId;
+        //    c.DateSubmitted = DateTime.Now;
+        //    m_requests.Add(c);
+        //}
+
+
+        //private AppDataContext _context;
+
+        public void RemoveRequest(int? id)
+        {
+
+        }
+
+        public void SetCompleted(int id)
+        {
+
+        }
+
+        public void Upvote(int id)
+        {
+
+        }
+
+        public void Downvote(int id)
+        {
+
+        }
+
+        public IQueryable<Subtitle> GetSubtitles()
+        {
+            //return _context;
+            return null;
+        }
+    }
+}
---
+++ SubHub/Repositories/SubtitleRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SubHub.Model;
+
+namespace SubHub.Repositories
+{
+    public class SubtitleRepository
+    {
+        private static SubtitleRepository _instance;
+
+        private static SubtitleRepository Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new SubtitleRepository();
+                return _instance;
+            }
+        }
+
+        private List<Subtitle> m_subtitles = null;
+
+        public IQueryable<Subtitle> GetSubtitles()
+        {
+            //var result = from c in m_subtitles
+            //             orderby dateSubmitted ascending
+            //             select c;
+            //return result;
+            return null;
+        }
+
+        public void AddSubtitle(Subtitle s)
+        {
+            int newId = 1;
+            if (m_subtitles.Count() > 0)
+            {
+                newId = m_subtitles.Max(x => x.Id) + 1;
+            }
+            s.Id = newId;
+            s.DateSubmitted = DateTime.Now;
+            m_subtitles.Add(s);
+        }
+        
+        public void DeleteSubtitle(int? id)
+        {
+            
+        }
+
+    }
+}
