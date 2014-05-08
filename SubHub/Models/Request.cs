58087cb5144867055aece92edd134de1aa3ce2cb
GudjonGeir <gudjongeir@gmail.com>
2014-05-08 10:50:34 +0000

    Finished relationships between UserAccounts and the other tables

--- SubHub/Models/Request.cs
+++ SubHub/Models/Request.cs
@@ -8,11 +8,12 @@ namespace SubHub.Models
 {
     public class Request
     {
-        public         int              Id              { get; set; }
-        public         string           Name            { get; set; }
-        public         DateTime         DateSubmitted   { get; set; }
-        public         bool             Completed       { get; set; }
-        // public         int              UserId          { get; set; }
-        public virtual RequestRating RequestRating { get; set; }   // Corresponding rating for the request
+        public         int             Id              { get; set; }
+        public         string          Name            { get; set; }
+        public         DateTime        DateSubmitted   { get; set; }
+        public         bool            Completed       { get; set; }
+        public         string          UserId          { get; set; }
+        public virtual ApplicationUser User            { get; set; }   
+        public virtual RequestRating   RequestRating   { get; set; }   // Corresponding rating for the request
     }
 }
